768ce8a7a4352654f1c2b6ccdb8ad30545132502
“zahrar-gholami” <zahra@cocurate.ai>
2025-02-08 04:39:16 +0900

    vite file structure fix

--- hosein-personal-website/vite.config.js
+++ hosein-personal-website/vite.config.js
@@ -1,15 +1,20 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html',
-        about: 'public/about.html',
-        contact: 'public/contact.html',
-        personal: 'public/personal.html',
-        professional: 'public/professional.html'
-      }
-    }
-  }
-}) 
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'public/about.html'),
+        contact: resolve(__dirname, 'public/contact.html'),
+        personal: resolve(__dirname, 'public/personal.html'),
+        professional: resolve(__dirname, 'public/professional.html'),
+        projects: resolve(__dirname, 'public/project-detail.html')
+      },
+    },
+  },
+})
